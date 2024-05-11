ccae9c6d1787a11d96b7eae7f2fa5d56d2ef9fc1
gv <rgarmartinez@protonmail.com>
2024-05-10 23:47:12 -0700

    adding files

---
+++ index.js
@@ -0,0 +1,48 @@
+const bttnDrum = document.querySelectorAll(".drum");
+
+const conditionCheck = (condition) => {
+  switch (condition) {
+    case "w":
+      const tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      const tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      const tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      const tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      const snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      const kickBass = new Audio("./sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+    case "l":
+      const crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
+  }
+};
+
+const handleClick = (e) => {
+  const listValue = e.target.innerHTML;
+  conditionCheck(listValue);
+};
+
+const keyPress = (e) => {
+  const keyCode = e.key;
+  conditionCheck(keyCode);
+};
+
+Array.from(bttnDrum).map((bttn) => bttn.addEventListener("click", handleClick));
+
+document.addEventListener("keydown", keyPress);
